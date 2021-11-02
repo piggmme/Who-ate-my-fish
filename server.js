e9bf5e8dda11897dde33060997e0f98c67e12d57
dbstjrrb12 <dbstjrrb1@gmail.com>
2021-11-02 13:19:23 +0900

    feat: Add socket event to modify user info & current user image

--- server.js
+++ server.js
@@ -20,9 +20,30 @@ app.get('/', (req, res) => {
   res.send('hi');
   // res.sendFile(__dirname + '/index.html');
 });
+const mapNameId = {};
+const CHARACTER = [
+  ['네로', './images/cats/cat6.png'],
+  ['오드아이', './images/cats/cat2.png'],
+  ['삼색이', './images/cats/cat3.png'],
+  ['치즈', './images/cats/cat4.png'],
+  ['샴', './images/cats/cat5.png'],
+];
 
+const civilUsers = [];
+
+const userUpdate = socketId => {
+  const userInfo = CHARACTER.shift();
+  civilUsers.push(userInfo);
+  mapNameId[userInfo[0]] = socketId;
+  io.to(socketId).emit('user update', userInfo);
+  io.emit('currentUsers', civilUsers);
+};
+
+// 들어올 때마다 모든 사람들한테 이벤트 방출해서 civilusers 제공!
 io.on('connection', socket => {
-  console.log('a user connected');
+  if (civilUsers.length < 5) {
+    userUpdate(socket.id);
+  }
 });
 
 server.listen(3000, () => {
--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,4 @@
-import axios from 'axios';
+// import axios from 'axios';
 import io from 'socket.io-client';
 
 const socket = io('http://localhost:3000');
@@ -10,5 +10,50 @@ const socket = io('http://localhost:3000');
 // };
 
 // fetchTodo();
+// axios.get('http://localhost:3000').then(resolve => console.log(resolve));
 
-axios.get('http://localhost:3000').then(resolve => console.log(resolve));
+// 단위 (ms)
+const STAGETIME = {
+  pending: 0,
+  beginning: 5000,
+  dayVote: 180000,
+  nightVote: 60000,
+};
+
+// ---------------------- pending ---------------------------
+// vote 버튼 비활성화, 싱태만 받아서 랜더링 진행
+// [{name : "네로", img_url: "/src/img-1.png" }]
+let currentUsers = [];
+const jailUsers = [];
+
+const renderUsers = () => {
+  const $filedset = document.querySelector('.info__users > fieldset');
+  $filedset.innerHTML = `
+  <legend>인원 ${currentUsers.length} / 5</legend>
+  ${currentUsers
+    .map(
+      (user, i) =>
+        `<label>
+            <input type="radio" id="user${i + 1}" name="user${i + 1}" />
+            <img src="${user[1]}" alt="플레이어 캐릭터" />
+            <span class="user-name">${user[0]}</span>
+        </label>`
+    )
+    .join('')}
+  `;
+};
+
+socket.on('user update', ([name, url]) => {
+  document.querySelector('.info__profile-name').textContent = name;
+  document.querySelector('.info__profile-img').setAttribute('src', url);
+});
+
+socket.on('currentUsers', civiluser => {
+  currentUsers = civiluser;
+  renderUsers();
+});
+
+// 타이머 설정 기능
+socket.on('timer setting', status => {
+  document.querySelector('.timer').innerHTML = '00:00';
+});
